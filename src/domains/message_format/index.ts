2ebbd791989e8aa69142b02e4c3e37294d1056bc
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2024-05-02 18:19:16 +0500

    upd

--- src/domains/message_format/index.ts
+++ src/domains/message_format/index.ts
@@ -2,16 +2,16 @@ import { Car } from '../../domains/scraper/scraper';
 
 export function stringifyCar(car: Car): string {
     const clearanceKZString = car.clearanceKZ ? 'Да' : 'Нет';
-    return `Детали машины:
+    return `  Детали машины:
     ID: ${car.id}
     Модель: ${car.name}
     Кузов: ${car.body}
     Город: ${car.city}
     Растаможен в Казахстане: ${clearanceKZString}
     Привод: ${car.drive}
-    Пробег: ${car.mileage}
-    Цена: ${car.price}
-    Объем: ${car.volume}
+    Пробег: ${car.mileage} км
+    Цена: ${car.price} тг
+    Объем: ${car.volume} л
     Ссылка: https://kolesa.kz/a/show/${car.id}
     `;
 }
