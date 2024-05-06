10593a55e6589e82650a66141943a0ab8a4c461c
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2024-05-06 18:13:29 +0500

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { ScraperService } from "./domains/scraper";
 import { CarService } from "./data/car";
-import { MongoDataBase } from "./db";
-// MongoDataBase.initMai`nDataBaseConnection();
 export async function main(url: string) {
-  // const url = 'https://kolesa.kz/a/show/155084003';
   const scraperService = new ScraperService();
   
   try {
